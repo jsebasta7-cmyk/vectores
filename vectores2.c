4da52df95e0860edc5130b2348e564d619fffeca
Laboratorio <jsebasta7@gmail.com>
2025-10-24 14:11:03 -0500

    Segundo commit

--- vectores2.c
+++ vectores2.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h> 
+#include <time.h>   
 #define MAX 100
 
-int main() {
-int Tipantuna[MAX] ={0};
-int tamano;
+int main()
+{
+    int Tipantuna[MAX];
+    int tamano;
 
-printf("Ingrese el tamaño del vector: ");
-scanf("%d", &tamano);
+    printf("Ingrese el tamaño del vector  ");
+    scanf("%d", &tamano);
 
-if (tamano > MAX || tamano <= 0) {
-printf("Tamaño inválido\n");
-return 1;
-}
+    if (tamano > MAX || tamano <= 0)
+    {
+        printf("Tamaño inválido.\n");
+        return 1;
+    }
 
+    srand(time(NULL));
 
-printf("\nVector Tipantuna:\n");
-for (int i = 0; i < tamano; i++) {
-printf("%d ", Tipantuna[i]);
-}
+    for (int i = 0; i < tamano; i++)
+    {
+        Tipantuna[i] = rand() % 100;
+    }
 
-printf("\n");
-return 0;
+    printf("\nVector Tipantuna :\n");
+    for (int i = 0; i < tamano; i++)
+    {
+        printf("%d ", Tipantuna[i]);
+    }
+
+    printf("\n");
+    return 0;
 }
